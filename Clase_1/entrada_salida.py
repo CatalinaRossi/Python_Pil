69c180f14d4c83721c4953382c11345e381b3661
CatalinaRossi <catalinaarossi@gmail.com>
2022-09-06 11:25:15 -0300

    Entrada y salida de datos

---
+++ Clase_1/entrada_salida.py
@@ -0,0 +1,15 @@
+print("Hello world!")
+print("Chauuuu world!")
+
+# Otras formas de print
+# Salto de linea
+print("esto es un salto \n de linea")
+# Tabulacion
+print("Con esto se \t tabula")
+# Concatenar
+print("Hola" + "Mundo", sep = " ")
+# Sep
+print("Hola", "Mundo", sep = " ")
+# End
+print("Hola", "Mundo", end = " ")
+print("Chau Mundo")
